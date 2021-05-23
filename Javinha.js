cadc30da47c037b93f3ea869f69414acbcd57397
oliveirafab16 <fabriciocarvalho932@gmail.com>
2021-05-23 19:38:50 -0300

    Media4NotasJS

---
+++ Javinha.js
@@ -0,0 +1,22 @@
+alert("Bora ver qual vai ser sua nota no final do bismestre kkkk ? :");
+
+let Desespero1= prompt("Primeira  ");
+let Desespero2= prompt("Segunda  ");
+let Desespero3= prompt("Terceira ");
+let Desespero4= prompt("Quarta  ");
+
+let Resultado = (Desespero1) + (Desespero2) + (Desespero3) + (Desespero4) / 4;
+
+if(Resultado <= 4){
+    alert(`Tu se LASCOU${Resultado}, você está reprovado`);
+}
+
+if(Resultado>=4 <= 5){
+    alert(`O caminho para o Céu foi cancelado temporariamente VIVA Outra Vez KKKK ${Resultado} Recuperação`);
+}
+
+if(Resultado >= 6){
+    alert(`O Golpe ta ai cai quem QUER🥰🐱‍🏍😂 ${Resultado}, você está aprovado`);
+}
+
+alert("“Corrija em particular, mas peça desculpas em público”!");
